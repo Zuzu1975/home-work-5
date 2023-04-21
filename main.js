9cafee358dbb5d4780fd2a0f79acc2b0568853e7
Zuzu work <muratov01@mail.ru>
2023-04-22 01:42:51 +0600

    занятие 5

---
+++ main.js
@@ -0,0 +1,17 @@
+
+             //домашка
+// for(let i=2500; i<3002; i++){
+//     if(i%2 ==0){                            
+//     console.log(i);
+//     }
+// }
+
+// let i =50;
+// while (i !=25){
+// alert(i);
+// i--;
+// }
+
+for(let i = 1; i<51; i++){
+        console.log(i);
+    }
